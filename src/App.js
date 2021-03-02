9d46abd4cea3a3c2e921a6c9194f533faaebd226
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-03-02 19:29:55 -0300

    Corrigindo erro

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+<<<<<<< HEAD
 import Feedback from './pages/Feedback';
+=======
+// import Feedback from './pages/Feedback';
+>>>>>>> 00daac418362281a1b48b7d54828ad9ff346d5cb
 import Trivia from './pages/Trivia';
 import Config from './pages/Config';
 
@@ -10,7 +14,11 @@ export default function App() {
   return (
     <Switch>
       <Route path="/trivia" component={ Trivia } />
+<<<<<<< HEAD
       <Route path="feedback" component={ Feedback } />
+=======
+      {/* <Route path="feedback" component={ Feedback } /> */}
+>>>>>>> 00daac418362281a1b48b7d54828ad9ff346d5cb
       <Route path="/config" component={ Config } />
       <Route exact path="/" component={ Login } />
     </Switch>
