8cac2e125dbae0ccc890c21003fb5a0d8357fe5b
Ana Karine <anakarine.ti@gmail.com>
2021-03-04 11:43:24 -0300

    tela de jogos

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import gravatarAPI from '../services/gravatarAPI';
 // import './Header.css';
+// coment
 
 export default class Header extends Component {
   render() {
