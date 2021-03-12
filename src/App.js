72c6931ce9f60b27bc60e95cc23c6dd595a30a53
Geziel <geziel.fernandez@pucp.pe>
2021-03-12 19:35:06 -0300

    some css working

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Play from './Pages/Play';
 import Settings from './Pages/Settings';
 import Feedback from './Pages/Feedback';
 import Ranking from './Pages/Ranking';
+import criatrivia from './images/criatrivia.png';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={ criatrivia } className="criatrivia" alt="criatrivia_group" />
         <Switch>
           <Route exact path="/">
             <Login />
